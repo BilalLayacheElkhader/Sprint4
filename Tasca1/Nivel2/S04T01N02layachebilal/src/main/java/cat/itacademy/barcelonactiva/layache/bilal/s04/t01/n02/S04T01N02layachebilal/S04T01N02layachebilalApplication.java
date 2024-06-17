78fc47088c87e7f22b413f7289ef5142f065993c
Bilal Layache <bilal.layache@gmail.com>
2024-06-17 11:30:50 +0200

    Tasca 1 Nivel 2

---
+++ Tasca1/Nivel2/S04T01N02layachebilal/src/main/java/cat/itacademy/barcelonactiva/layache/bilal/s04/t01/n02/S04T01N02layachebilal/S04T01N02layachebilalApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.layache.bilal.s04.t01.n02.S04T01N02layachebilal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02layachebilalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02layachebilalApplication.class, args);
+	}
+
+}
